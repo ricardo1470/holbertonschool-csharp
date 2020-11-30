a7e5d25c3e908382150dd9be1441d4e08be71f67
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-11-30 15:16:47 -0500

    print the given variables as in the result shown

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -7,6 +7,6 @@ class Program
 		double percent = .7553;
 		double currency = 98765.4321;
 		Console.WriteLine("Percent: {0:00.00%}", percent);
-		Console.WriteLine("Currency: {0:C} en-US.utf-8", currency);
+		Console.WriteLine(en-US.utf-8"Currency: {0:C}", currency);
 	}
 }
