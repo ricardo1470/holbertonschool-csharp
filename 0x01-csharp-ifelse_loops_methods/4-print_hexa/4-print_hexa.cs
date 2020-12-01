3d7111cef92dee80ef7f1153e5b890329a6b4199
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-01 15:31:07 -0500

    prints all numbers from 0 to 98 in decimal and in hexadecimal

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            for (number = 0 ; number <= 98 ; number++)
+            {
+                Console.WriteLine("{0} = 0x{0:x}", number);
+            }
+        }
+    }
+}
