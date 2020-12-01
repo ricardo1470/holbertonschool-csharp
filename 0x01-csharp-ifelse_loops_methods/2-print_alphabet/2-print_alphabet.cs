36b7241468e63e14eb6d19635bd10fe1e8f879cb
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-01 14:42:27 -0500

    prints the alphabet, in lowercase

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char alphabet;
+
+            for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+            {
+                Console.Write(alphabet);
+            }
+        }
+    }
+}
