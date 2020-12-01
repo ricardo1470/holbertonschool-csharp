cde0dde1377b5d068bd3a72a29e03903f85822bd
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-01 14:55:30 -0500

    prints the alphabet, in lowercase
    except q and e

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char alphabet;
+
+            for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+            {
+                if (alphabet != 'q' && alphabet != 'e')
+                {
+                    Console.Write(alphabet);
+                }
+            }
+        }
+    }
+}
