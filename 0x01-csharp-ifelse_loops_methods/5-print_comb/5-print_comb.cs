f58588e4b0b4cbff4ad4d1f6e3929102bdb24bd2
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-01 16:17:27 -0500

    prints numbers from 0 to 99.

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            string  comma = "";
+
+            for (number = 0 ; number <= 99 ; number++)
+            {
+                Console.Write("{0}{1:d2}", comma, number);
+                comma = ", ";
+            }
+        }
+    }
+}
