5a8aaec5ae7d975f14fffa8de2e039360bac33b4
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-02 17:38:22 -0500

    prints an array of integers of a given size

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        int i;
+
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+
+        int[] numbers = new int[size];
+        
+        for (i = 0; i < size; i++)
+        {
+            numbers[i] = i;
+        }
+        Console.WriteLine("{0}", String.Join(" ", numbers));
+        return numbers;
+    }
+}
