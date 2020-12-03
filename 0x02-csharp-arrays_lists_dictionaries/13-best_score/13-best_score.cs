d49315d3af6fd89179fad44378a7fa7b6637ba5e
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-02 23:57:51 -0500

    create files

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	public static string BestScore(Dictionary<string, int> myList)
+	{
+		int max = 0;
+		string result = "None";
+		if (myList.Count <= 0)
+		{
+			return result;
+		}
+		foreach (KeyValuePair<string, int> kvp in myList)
+		{
+			if (max <= kvp.Value)
+			{
+				max = kvp.Value;
+				result = kvp.Key;
+			}
+		}
+		return result;
+	}
+}
