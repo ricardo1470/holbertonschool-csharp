cab404bb0328ac9d22f3338a03690fe16d6f1d3e
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-02 23:59:34 -0500

    create

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int[,] array = new int[5, 5];
+		array[2, 2] = 1;
+		for (int i = 0; i < 5; i++)
+		{
+			Console.WriteLine("{0} {1} {2} {3} {4}", array[i, 0], array[i, 1], array[i, 2], array[i, 3], array[i, 4]);
+		}
+	}
+}
