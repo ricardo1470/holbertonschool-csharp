3c5dc4cd7cb0bc431ed08ee8e2ebc4b4400fdbc4
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-02 23:54:23 -0500

    create files

--- 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 
 class Dictionary
 {
-	public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
-	{
-		myDict.Remove(key);
-		return (myDict);
-	}
+    public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
+    {
+        if (myDict.ContainsKey(key))
+            myDict.Remove(key);
+
+        return (myDict);
+    }
 }
