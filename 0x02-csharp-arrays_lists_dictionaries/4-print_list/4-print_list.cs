bd3a42249c4047b926f139c5073ad803e13eb7aa
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-02 22:12:33 -0500

    prints a list of integers of a given size.

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        int i;
+
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> my_List = new List<int>();
+
+        for (i = 0; i < size; i++)
+        {
+            my_List.Add(i);
+            Console.Write(i);
+            if (i < size - 1)
+            {
+                Console.Write(' ');
+            }
+        }
+        Console.WriteLine();
+        return my_List;
+    }
+}
