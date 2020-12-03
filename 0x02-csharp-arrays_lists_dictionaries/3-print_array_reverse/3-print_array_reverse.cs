7b82629e7e37b1a580a85660d0426064075cc277
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-02 19:38:18 -0500

    prints all integers of an array, in reverse order.

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        int i;
+
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+        for (i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write(array[i]);
+            if (i != 0)
+            {
+                Console.Write(' ');
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
