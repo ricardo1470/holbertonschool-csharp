495b33c1cefb11b4bb2bb3d9e96fe25662d74563
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-02 22:46:09 -0500

    method that finds all multiples of 2 in a list.

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> resultList = new List<bool>();
+        if (myList.Count == 0)
+        {
+            return resultList;
+        }
+        foreach (int element in myList)
+        {
+            if (element % 2 == 0)
+            {
+                resultList.Add(true);
+            }
+            else
+            {
+                resultList.Add(false);
+            }
+        }
+        return resultList;
+    }
+}
