1b777ee00e7cdee24e93885a5eb0348cfdee2b24
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-03 22:45:17 -0500

    create files

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+	public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+	{
+		Console.WriteLine("Number of items: {0}", aQueue.Count);
+		if (aQueue.Count <= 0)
+		{
+			Console.WriteLine("Queue is empty");
+		}
+		else
+		{
+			Console.WriteLine("First item: {0}", aQueue.Peek());
+		}
+		if (aQueue.Contains(search))
+		{
+			Console.WriteLine("Queue contains \"{0}\": True", search);
+			while (aQueue.Dequeue() != search) ;
+		}
+		else
+		{
+			Console.WriteLine("Queue contains \"{0}\": False", search);
+		}
+		aQueue.Enqueue(newItem);
+		return aQueue;
+	}
+}
