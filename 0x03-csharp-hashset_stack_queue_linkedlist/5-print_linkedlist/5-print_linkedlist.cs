41d51fe6ffe3465135fc4091d92d07ea94e35562
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-03 22:49:34 -0500

    method that creates and prints a LinkedList of integers of a given size.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedList<int> CreatePrint(int size)
+	{
+        int i;
+        
+		LinkedList<int> my_LList = new LinkedList<int>();
+		if (size <= 0)
+		{
+			return my_LList;
+		}
+		for (i = 0; i < size; i++)
+		{
+			Console.WriteLine(i);
+			my_LList.AddLast(i);
+		}
+		return my_LList;
+	}
+}
