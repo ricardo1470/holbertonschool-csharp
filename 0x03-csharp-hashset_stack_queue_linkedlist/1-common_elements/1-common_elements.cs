88e5851e295b95b72c9d582307609bd70ad54db5
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-03 22:31:01 -0500

    method that returns a sorted list of common elements in two lists.

--- 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -5,7 +5,7 @@ class List
 {
     public static List<int> CommonElements(List<int> list1, List<int> list2)
     {
-        List<int> myList = list1.FindAll(elem => list2.Contains(elem));
-        return myList;
+        List<int> item = list1.FindAll(elem => list2.Contains(elem));
+        return (item);
     }
 }
