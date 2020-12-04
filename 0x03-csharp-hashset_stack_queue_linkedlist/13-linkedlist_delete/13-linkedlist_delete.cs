25865ddd0ec46075e497f0b8523c79ef7997c304
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-03 23:36:53 -0500

    method that deletes the node at given position in a LinkedList.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int i;
+
+        LinkedListNode<int> tmp = myLList.First;
+        if (index >= myLList.Count || index < 0)
+        {
+            return;
+        }
+        for (i = 0; i < index; i++)
+        {
+            tmp = tmp.Next;
+        }
+        myLList.Remove(tmp);
+    }
+}
