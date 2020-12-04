ea845f6b207fd13dcdf458c532fbfa25a1d0fdb2
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-03 23:26:37 -0500

    method that returns the value of the nth node of a LinkedList.

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int GetNode(LinkedList<int> myLList, int n)
+	{
+		int idx = 0;
+		foreach (int element in myLList)
+		{
+			if (idx == n)
+			{
+				return (element);
+			}
+			idx++;
+		}
+		return (0);
+	}
+}
