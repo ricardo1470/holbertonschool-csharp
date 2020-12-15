61114069859154cf2d6a6e2a1d8cf0afe55337be
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-14 23:23:17 -0500

    override the .ToString() method to print the Dog object’s attributes

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public enum Rating { Good, Great, Excellent };
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string dName, float dAge, string dOwner, Rating dRating)
+    {
+        name = dName;
+        age = dAge;
+        owner = dOwner;
+        rating = dRating;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("Dog Name: {0}\nAge: {1}\nOwner: {2}\nRating: {3}", name, age, owner, rating);
+    }
+}
