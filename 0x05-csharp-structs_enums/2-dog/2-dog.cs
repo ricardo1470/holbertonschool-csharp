5075c8f311e83fccbf9f763ef7101e2992065700
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-14 23:20:07 -0500

    add a constructor to struct Dog

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public enum Rating { Good, Great, Excellent };
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string dName, float dAge, string dOwner, Rating dRating)
+    {
+        name = dName;
+        age = dAge;
+        owner = dOwner;
+        rating = dRating;
+    }
+}
