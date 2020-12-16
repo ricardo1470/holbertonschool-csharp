2fd538527a2f888f291560fb62cc37419c8a7144
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-15 22:39:49 -0500

    public class Zombie that defines a zombie

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace Enemies
+{
+    /// <summary>public class Zombie that defines a zombie</summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Health of the zombie
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        ///Zombie health
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <sumary>
+        ///public constructor
+        ///</sumary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+        /// <sumary>
+        ///public method
+        ///</sumary>
+        public int GetHealth()
+        {
+            return (health);
+        }
+    }
+}
