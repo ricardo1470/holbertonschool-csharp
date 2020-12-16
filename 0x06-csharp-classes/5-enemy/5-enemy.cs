12e9f0ee6d762b506abb68ea5d1cf040458f5fd8
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-15 22:51:14 -0500

    public class Zombie that defines a zombie

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace Enemies
+{
+    /// <summary>public class Zombie that defines a zombie</summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Health of the zombie
+        /// </summary>
+        private int health;
+        private string name = "(No name)";
+
+        /// <summary>
+        ///Zombie health
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <sumary>
+        ///public constructor
+        ///</sumary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+        /// <sumary>
+        ///public method
+        ///</sumary>
+        public int GetHealth()
+        {
+            return (health);
+        }
+        /// <sumary>
+        ///public property
+        ///</sumary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        /// <sumary>
+        ///public method
+        ///</sumary>
+
+        public override string ToString()
+        {
+            return string.Format("Zombie Name: {0} / Total Health: {1}", name, health);
+        }
+    }
+}
