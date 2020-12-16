9337ec55a2dfb212a230ec9969f33363ec853d31
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-15 22:32:09 -0500

    public class Zombie that defines a zombie

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Enemies
+{
+    /// <summary>public class Zombie that defines a zombie</summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Health of the zombie
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        ///Zombie health
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
+
+
