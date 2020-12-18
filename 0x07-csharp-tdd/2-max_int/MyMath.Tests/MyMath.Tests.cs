b160320e29fa7210ca8fbf927c14d2aa5eba6815
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-17 22:59:13 -0500

    max integer

---
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    [TestFixture]
+	public class OperationsTests
+	{
+		[Test]
+		public void Test1()
+		{
+			List<int> array = new List<int>();
+			array.Add(4);
+			array.Add(2);
+			array.Add(-4);
+			array.Add(5);
+			array.Add(1);
+
+			int output = Operations.Max(array);
+
+			Assert.AreEqual(5, output);
+		}
+
+		[Test]
+		public void Test2()
+		{
+			List<int> array = new List<int>();
+
+			int output = Operations.Max(array);
+
+			Assert.AreEqual(0, output);
+		}
+
+		[Test]
+		public void Test3()
+		{
+			List<int> array = null;
+
+			int output = Operations.Max(array);
+
+			Assert.AreEqual(0, output);
+		}
+	}
+}
--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -2,7 +2,28 @@
 
 namespace MyMath
 {
-    public class Class1
+    /// <summary>
+    /// method that returns the max integer in a list of integers.
+    /// </summary>
+    public class Operations
     {
+        /// <summary>
+        /// method that returns max integer in a list
+        /// </summary>
+        public static int Max(List<int> nums)
+        {
+            int i = 0;
+            if (nums.Count > 0)
+            {
+                foreach (int num in nums)
+                {
+                    if (num > i)
+                    {
+                        i = num;
+                    }
+                }
+            }
+            return i;
+        }
     }
 }
