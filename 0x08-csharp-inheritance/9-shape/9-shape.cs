42af1b5dc42bd901b47c4975d1e94acd95162e4f
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-21 22:59:54 -0500

    method within your Square class

--- 0x08-csharp-inheritance/9-shape/9-shape.cs
+++ 0x08-csharp-inheritance/9-shape/9-shape.cs
@@ -1,12 +1,102 @@
 ﻿using System;
 
-namespace _9_shape
+/// <summary>a class Shape.</summary>
+class Shape
 {
-    class Program
+    /// <summary>public method: public virtual int Area()</summary>
+    public virtual int Area()
     {
-        static void Main(string[] args)
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>a class Rectangle that inherits from Shape</summary>
+class Rectangle : Shape {
+    ///<summary>private int width </summary>
+    private int width;
+
+    ///<summary>private int height </summary>
+    private int height;
+
+    /// <summary>public int Width</summary>
+    public int Width {
+        get
         {
-            Console.WriteLine("Hello World!");
+            return (width);
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            else
+            {
+                width = value;
+            }
         }
     }
+
+    /// <summary>public property</summary>
+    public int Height {
+        get
+        {
+            return (height);
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            else
+            {
+                height = value;
+            }
+        }
+    }
+
+    /// <summary>will override the Area() method defined in the Shape base class.</summary>
+    public new int Area()
+    {
+        return (height * width);
+    }
+
+    /// <summary>public override string ToString().</summary>
+    public override string ToString()
+    {
+        return (string.Format("[Rectangle] {0} / {1}", width, height));
+    }
+}
+
+/// <summary>write a class Square that inherits from Rectangle</summary>
+class Square : Rectangle {
+    /// <summary>private field</summary>
+    private int size;
+
+    /// <summary>public property</summary>
+    public int Size
+    {
+        get
+        {
+            return (size);
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            }
+            else
+            {
+                size = Height = Width = value;
+            }
+        }
+    }
+
+    /// <summary>method within your Square class</summary>
+    public override string ToString()
+	{
+		return String.Format("[Square] {0} / {0}", size);
+	}
 }
