6e0b2b91f1aa18476a742158ae2c6a6d785d1dd0
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-21 22:21:41 -0500

    write a class Rectangle that inherits from Shape

--- 0x08-csharp-inheritance/6-shape/6-shape.cs
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -1,12 +1,63 @@
 ﻿using System;
 
-namespace _6_shape
+/// <summary>
+/// a class Shape.
+/// </summary>
+class Shape
 {
-    class Program
+    /// <summary>
+    /// public method: public virtual int Area()
+    /// </summary>
+    public virtual int Area()
     {
-        static void Main(string[] args)
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>
+/// a class Rectangle that inherits from Shape
+/// </summary>
+class Rectangle : Shape {
+    ///<summary>private int width </summary>
+    private int width;
+    ///<summary>private int height </summary>
+    private int height;
+    /// <summary>
+    /// public int Width
+    /// </summary>
+    public int Width {
+        get
         {
-            Console.WriteLine("Hello World!");
+            return (width);
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            else
+            {
+                width = value;
+            }
+        }
+    }
+    /// <summary>public property</summary>
+    public int Height {
+        get
+        {
+            return (height);
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            else
+            {
+                height = value;
+            }
         }
     }
 }
