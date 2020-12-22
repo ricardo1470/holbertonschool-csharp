44475aa3f7ae75be34ae2cf86c587fd40aa18f0e
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-21 19:02:39 -0500

    returns True if the object is an instance of a class

--- 0x08-csharp-inheritance/2-subclass/2-subclass.cs
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -14,10 +14,6 @@ class Obj
     /// </summary>
     public static bool IsOnlyASubclass(Type derivedType, Type baseType)
     {
-        if (derivedType.BaseType == baseType)
-        {
-            return (true);
-        }
-        return (false);
+        return derivedType.IsSubclassOf(baseType);
     }
 }
