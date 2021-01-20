807f814e74b41fea29eb8d3815994564bab48b36
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-01-20 02:00:35 -0500

    method that adds two vectors and returns the resulting vector.

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -13,17 +13,16 @@ class VectorMath
     /// <return> return a vector containing -1 </return>
     public static double[] Add(double[] vector1, double[] vector2)
     {
-        Vector vectorResult = new Vector();
-        
         if (vector1.Length != vector2.Length || vector1.Length < 2 || vector1.Length > 3
         || vector2.Length < 2 || vector2.Length > 3)
         {
+            double[] vectorResult = new Vector(); 
+
             for (int i = 0; i < vector1.Length; i++)
             {
                 vectorResult = Vector.Add(vector1, vector2);
             }
             return vectorResult;
-
         }
         else  { return new double[]{-1}; }
     }
