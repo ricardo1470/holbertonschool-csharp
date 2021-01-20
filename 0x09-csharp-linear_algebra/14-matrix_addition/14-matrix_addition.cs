2351931447b471b53647ba73499de2a4f0604951
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-01-20 16:25:48 -0500

    method that adds two matrices and returns the resulting matrix.

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -17,14 +17,18 @@ class MatrixMath
     {
         int i, j;
 
-        if (matrix1.Length  =! matrix2.Length)
+        if (matrix1.GetLength(0) != matrix2.GetLength(0) ||
+        || matrix1.GetLength(0) < 2|| matrix1.GetLength(0) > 3
+        || matrix2.GetLength(0) < 2|| matrix2.GetLength(0) > 3
+        || matrix1.GetLength(1) < 2|| matrix1.GetLength(1) > 3
+        || matrix2.GetLength(1) < 2|| matrix2.GetLength(1) > 3)
         {
             return new double [,] {{-1}};
         }
 
-        for (i = 0; i < matrix1.Length; i++)
+        for (i = 0; i < matrix1.GetLength(0); i++)
         {
-            for (j = 0; j < matrix2.Length; j++)
+            for (j = 0; j < matrix2.GetLength(1); j++)
             {
                 sum_matrix[i,j] = Math.Round(matrix1[i,j] + matrix2[i,j], 2);
             }
