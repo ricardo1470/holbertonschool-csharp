2aaaa8782d62af19b3239921aaa3fa3f8a68005e
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-01-22 00:25:19 -0500

    method that multiplies two matrices and returns the resulting matrix.

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -17,7 +17,6 @@ class MatrixMath
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
         double[,] mult_matrix = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
-        double[,] mult_matrix2 = matrix2.GetLength(1);
 
         if (matrix1.GetLength(1) != matrix2.GetLength(0))
         {
@@ -26,7 +25,7 @@ class MatrixMath
 
         for (int i = 0; i < mult_matrix.GetLength(0); i++)
         {
-            for (int j = 0; j < mult_matrix2.GetLength(1); j++)
+            for (int j = 0; j < mult_matrix.GetLength(1); j++)
             {
                 double temp = 0;
                 for (int k = 0; k < mult_matrix.GetLength(0); k++)
