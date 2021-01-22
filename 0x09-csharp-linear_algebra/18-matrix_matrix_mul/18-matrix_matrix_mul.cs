cbc58e1c8a4da04cced0e6f1014343c649f5f950
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-01-21 23:23:15 -0500

    method that multiplies two matrices and returns the resulting matrix.

--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -1,42 +1,39 @@
 ﻿using System;
 
-namespace _18_matrix_matrix_mul
+/// <sumary>
+/// method that multiplies two matrices,
+/// and returns the resulting matrix.
+/// </sumary>
+class MatrixMath
 {
-}
     /// <sumary>
-    /// method that multiplies two matrices,
-    /// and returns the resulting matrix.
+    /// The matrices will not necessarily
+    /// be square or the same dimensions
+    /// If the matrices cannot be multiplied,
     /// </sumary>
-    class MatrixMath
+    /// <return>
+    /// return a matrix containing -1
+    /// </return>
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
-        /// <sumary>
-        /// The matrices will not necessarily
-        /// be square or the same dimensions
-        /// If the matrices cannot be multiplied,
-        /// </sumary>
-        /// <return>
-        /// return a matrix containing -1
-        /// </return>
-        public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
-        {
-            double [,] mult_matrix = new double [matrix2.GetLength(0), matrix2.GetLength(1)];
-            double temp = 0;
+        double [,] mult_matrix = new double [matrix2.GetLength(0), matrix2.GetLength(1)];
+        double temp = 0;
 
-            if (matrix1.GetLength(1) == matrix2.GetLength(0))
+        if (matrix1.GetLength(1) == matrix2.GetLength(0))
+        {
+            for (int i = 0; i < mult_matrix.GetLength(0); i++)
             {
-                for (int i = 0; i < mult_matrix.GetLength(0); i++)
+                for (int j = 0; j < mult_matrix.GetLength(1); j++)
                 {
-                    for (int j = 0; j < mult_matrix.GetLength(1); j++)
+                    for (int k = 0; k < mult_matrix.GetLength(0); k++)
                     {
-                        for (int k = 0; k < mult_matrix.GetLength(0); k++)
-                        {
-                            temp += matrix1[i, k] * matrix2[k, j];
-                        }
-                        mult_matrix[i, j] = Math.Round(temp, 2);
+                        temp += matrix1[i, k] * matrix2[k, j];
                     }
+                    mult_matrix[i, j] = Math.Round(temp, 2);
                 }
-                return (mult_matrix);
             }
-            return new double [,] {{-1}};
+            return (mult_matrix);
         }
+        return new double [,] {{-1}};
     }
+}
