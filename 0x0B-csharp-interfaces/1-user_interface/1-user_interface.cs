00c655b2d56971ec3fdaabf9858a13d96db25961
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-03-23 13:06:35 -0500

    update files

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,62 +1,65 @@
 ﻿using System;
 
-abstract class Base
+/// <summary>
+/// abstract class called Base
+/// </summary>
+public abstract class Base
 {
-    public string name = null;
+    /// <summary>property</summary>
+    public string name { get; set; }
 
-    /// <summary>
-    /// abstract class called Base
-    /// </summary>
+    /// <summary>abstract class called Base</summary>
     public override string ToString()
     {
-        return name + " is a " + this.GetType();
+        return this.name + " is a " + this.GetType();
     }
 }
 
-
-/// <summary>
-/// an interface called IInteractive
-/// </summary>
-interface  IInteractive
+/// <summary>an interface called IInteractive</summary>
+public interface  IInteractive
 {
+    /// <summary>method Interact</summary>
     void Interact();
 }
 
-/// <summary>
-///  an interface called IBreakable
-/// </summary>
-interface IBreakable
+/// <summary>an interface called IBreakable</summary>
+public interface IBreakable
 {
+    /// <summary>property durability</summary>
     int durability { get; set; }
+    /// <summary>method Break</summary>
     void Break();
 }
 
-/// <summary>
-///  an interface called IBreakable
-/// </summary>
-interface ICollectable
+/// <summary>an interface called IBreakable</summary>
+public interface ICollectable
 {
+    /// <summary>property isCollected</summary>
     bool isCollected { get; set; }
+    /// <summary>method Collect</summary>
     void Collect();
 }
 
-/// <summary>
-/// new class called TestObject
-/// </summary>
-class TestObject : Base, IInteractive, IBreakable, ICollectable
+/// <summary>new class called TestObject</summary>
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
+    ///<summary>Property durability</summary>
     public int durability { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    ///<summary>Property isCollected</summary>
     public bool isCollected { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
+    ///<summary>Method Interact</summary>
     public void Interact()
     {
         throw new NotImplementedException();
     }
+    ///<summary>Method Break</summary>
     public void Break()
     {
         throw new NotImplementedException();
     }
 
+    ///<summary>Method Collect</summary>
     public void Collect()
     {
         throw new NotImplementedException();
